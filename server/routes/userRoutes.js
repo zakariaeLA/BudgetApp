a79cc080b09e21365bd540d0681cda88d6c58d68
zakariaeLA <lachhabzakariae@gmail.com>
2025-01-06 14:00:14 +0000

    saisit de l'argent.. et dahboard

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -51,7 +51,7 @@ router.post("/register", async (req, res) => {
           console.error(err);
           return res.status(500).send("Erreur lors de l'inscription");
         }
-        res.send("Utilisateur inscrit avec succès");
+        res.json({ userId: result.insertId });
       }
     );
   });
@@ -61,30 +61,51 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   const { email, motdepasse } = req.body;
 
-  // Vérifier si l'email existe
+  // Récupérer l'utilisateur par email
   const sql = "SELECT * FROM utilisateur WHERE email = ?";
-  db.query(sql, [email], async (err, results) => {
+  db.query(sql, [email], (err, results) => {
     if (err) {
-      console.error(err);
       return res.status(500).send("Erreur lors de la connexion");
     }
-
-    // Si aucun utilisateur n'est trouvé avec cet email
-    if (results.length === 0) {
-      return res.status(404).send("Email non trouvé");
-    }
-
-    // Comparer le mot de passe
-    const isValidPassword = await bcrypt.compare(
-      motdepasse,
-      results[0].motdepasse
-    );
-    if (isValidPassword) {
-      res.send("Connexion réussie");
+    if (results.length > 0) {
+      const user = results[0];
+      // Vérifier le mot de passe haché
+      bcrypt.compare(motdepasse, user.motdepasse, (err, isMatch) => {
+        if (err) {
+          return res
+            .status(500)
+            .send("Erreur lors de la vérification du mot de passe");
+        }
+        if (isMatch) {
+          // Renvoyer l'ID de l'utilisateur et un message de succès
+          res.json({ userId: user.id, message: "Connexion réussie" });
+        } else {
+          res.status(401).send("Identifiants incorrects");
+        }
+      });
     } else {
-      res.status(401).send("Mot de passe incorrect");
+      res.status(401).send("Identifiants incorrects");
     }
   });
+});
+
+// Endpoint pour ajouter des données à la table Argent
+router.post("/argent", (req, res) => {
+  const { idutilisateur, totalargent, salaire, budget, epargne } = req.body;
+
+  const sql =
+    "INSERT INTO argent (idutilisateur, totalargent, salaire, budget, epargne) VALUES (?, ?, ?, ?, ?)";
+  db.query(
+    sql,
+    [idutilisateur, totalargent, salaire, budget, epargne],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Erreur lors de l'ajout des données");
+      }
+      res.send("Données ajoutées avec succès");
+    }
+  );
 });
 
 module.exports = router;
